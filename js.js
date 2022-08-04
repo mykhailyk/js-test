b810cceb4117cf9c0404c4a0dcc1c5b608e715f1
mykhailyk <104762837+mykhailyk@users.noreply.github.com>
2022-08-04 23:18:10 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,15 +1,12 @@
-function filterArray(numbers, value) {
+function calculateTotalPrice(order) {
+  let total = 0;
   // Change code below this line
-  numbers.sort(function (a, b) {
-    return a - b;
-  });
-  const num = [];
-  let end = numbers[numbers.length - 1];
-  for (let i = value + 1; i <= end; i += 1) {
-    num.push(i);
+  for (let ord of order) {
+    // for (let i = 0; i < order.length; i += 1) {
+    total += ord;
   }
-  return num;
-  // Change code above this line
-}
 
-console.log(filterArray([12, 24, 8, 41, 76], 38));
+  // Change code above this line
+  return total;
+}
+console.log(calculateTotalPrice([12, 85, 37, 4]));

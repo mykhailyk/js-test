a89bf04d16172cd4f8c1141f72998ade732505bb
mykhailyk <104762837+mykhailyk@users.noreply.github.com>
2022-08-06 12:25:07 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,8 +1,23 @@
-const salary = prompt("введіть Вашу заробітну платню:");
-let addSalary = salary * 1.15; //додаткові 15% премія
-alert(`зарплата з премією = ${addSalary}`);
-let vat = addSalary * 0.9; //податок 10%
-alert(`зарплата з вирахуванням податків  = ${vat}`);
-let spend = vat - 190; // витрати в маг
-alert(`залишилося на дружину  = ${spend / 2}`);
-alert(`залишилося на чоловіка  = ${spend / 2}`);
+function checkTicket(number) {
+  let leftSide = 0;
+  let rightSide = 0;
+  for (let i = 0; i < number.length; i += 1) {
+    if (i < number.length / 2) {
+      leftSide += Number(number[i]);
+      continue;
+    }
+    rightSide += Number(number[i]);
+  }
+  console.log(leftSide);
+  console.log(rightSide);
+  // if (leftSide === rightSide) {
+  //   return true;
+  // }
+  // return false;
+
+  return leftSide === rightSide;
+}
+
+console.log(checkTicket("005212"));
+console.log(checkTicket("133700"));
+console.log(checkTicket("123032"));

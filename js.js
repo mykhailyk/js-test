354a848a361360dec770172ab87de8ea25247f72
mykhailyk <104762837+mykhailyk@users.noreply.github.com>
2022-08-07 13:55:21 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,23 +1,34 @@
-function checkTicket(number) {
-  let leftSide = 0;
-  let rightSide = 0;
-  for (let i = 0; i < number.length; i += 1) {
-    if (i < number.length / 2) {
-      leftSide += Number(number[i]);
-      continue;
-    }
-    rightSide += Number(number[i]);
+let count = 0;
+
+function cc(card) {
+  // Змініть код лише під цим рядком
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
   }
-  console.log(leftSide);
-  console.log(rightSide);
-  // if (leftSide === rightSide) {
-  //   return true;
-  // }
-  // return false;
+  console.log(count);
+  let holdbet = "Hold";
+  if (count > 0) {
+    holdbet = "Bet";
+  }
 
-  return leftSide === rightSide;
+  return count + " " + holdbet;
+  // Змініть код лише над цим рядком
 }
-
-console.log(checkTicket("005212"));
-console.log(checkTicket("133700"));
-console.log(checkTicket("123032"));
+console.log(cc(2, 3, 4, 5, 6));
+console.log(cc(10, J, Q, K, A));
+console.log(cc(3, 2, A, 10, K));
+console.log(cc(7, 8, 9));

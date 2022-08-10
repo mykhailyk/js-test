88178a60d3e815e047849e75a1dd28651de0a40d
Yurii Mikhaylik <imxomunn@gmail.com>
2022-08-10 17:22:29 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,3 +1,27 @@
+// Перебери об'єкт apartment, використовуючи цикл for...in,
+// і запиши в масив keys всі його ключі, а в масив values
+// всі значення його властивостей.
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const keys = [];
+// const values = [];
+// for (let key in apartment) {
+//   console.log(key);
+//   console.log("_________");
+//   keys.push(key);
+//   values.push(apartment[key]);
+// }
+// console.log(keys);
+// console.log(values);
+
+// Напиши функцію calculateTotalPrice(productName),
+//   яка приймає один параметр productName - назва товару.
+//  Функція повинна повернути загальну
+// вартість(ціна * кількість) товару з таким ім'ям з
+// масиву products.
 const products = [
   { name: "Radar", price: 1300, quantity: 4 },
   { name: "Scanner", price: 2700, quantity: 3 },
@@ -5,25 +29,17 @@ const products = [
   { name: "Grip", price: 1200, quantity: 9 },
 ];
 
-function getAllPropValues(propName) {
+function calculateTotalPrice(productName) {
   // Change code below this line
-  const mass = [];
-  const product = Object.keys(products);
-  for (let prod of product) {
-    console.log(products[prod].name);
-    console.log(propName);
-    if (products[prod].name === propName) {
-      mass.push(apartment[key]);
-    }
-    // console.log(products[prod].propName);
-  }
+  const total = 0;
 
+  for (const key of products) {
+    console.log(key[productName]);
+    if (key[productName]) {
+      total = products.price * products.quantity;
+    }
+  }
+  return total;
   // Change code above this line
 }
-
-console.log(getAllPropValues("name"));
-console.log(getAllPropValues("quantity"));
-
-// for (const key of keys) {
-//   // keys.push(key);
-//   values.push(apartment[key].name);
+console.log(calculateTotalPrice("Radar"));
